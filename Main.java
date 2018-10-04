130c742d0db908f7abf79ef3f3547bcde455c1a6
n3legal <nikita.shachnov@yandex.ru>
2018-10-04 20:09:36 +0300

    1.2

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.lang.String;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Amount element of args: " + args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i] + "  ");
+        }
+    }
+}
